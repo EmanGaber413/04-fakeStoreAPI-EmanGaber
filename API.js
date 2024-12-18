e11b447a628b09a89ec372f16a0b9dbb1d63bb1c
Eman Gaber <emangaber413@gmail.com>
2024-12-18 21:25:53 +0200

    slider

--- API.js
+++ API.js
@@ -1,37 +1,97 @@
 
+var JsonData =''
+var XmlHttp = new XMLHttpRequest();
 function api(url,method='GET') {
-    var XmlHttp = new XMLHttpRequest();
     XmlHttp.open(method, url);
     XmlHttp.send();
     XmlHttp.addEventListener('readystatechange', function () {
         if (XmlHttp.readyState===4 && XmlHttp.status===200) 
-            { var JsonData =JSON.parse(XmlHttp.responseText)
-            var htmlPostView=document.getElementById('PostData')  
+            {  JsonData =JSON.parse(XmlHttp.responseText)
+             var htmlPostView=document.getElementById('PostData')  
              var HtmlCollection=``
              for (var post of JsonData) {
                 HtmlCollection +=
                         `<div class=" card col-3 my-4" style="width: 18rem;">
-                        <img src="${post.image}" class="card-img-top p-4 w-100 " style="height: 250px;" >
+                         <img src="${post.image}" class="card-img-top p-4 w-100 " style="height: 250px;" >
                          <div  class="card-body">
                              <h5 class="card-title">${post.title}</h5>
 
                              <div  class="d-flex justify-content-center">
-                             <h4 class="card-title text-danger col-6 fw-bolder border border-2 
-                             border-danger text-center bg-body-tertiary ">${post.price}</h4>
+                             <h4 class="card-title text-black col-6 fw-bolder border border-2 
+                             border-black text-center bg-body-tertiary cc"  onclick="ApplyRed(this)"
+                              >${post.price} $</h4>
                              </div>
 
                              <p class="card-text">${post.description}</p>
                              <a href="#" class="btn btn-info text-center w-100 justify-item-around">Add to Cart </a>
                          </div>
-                         </div>`
-               htmlPostView.innerHTML=HtmlCollection;
-            }}
+                         </div>
+                         
+                         `
+                         
+              
+
+              }
+              htmlPostView.innerHTML=HtmlCollection;
+         }  
         
     })
+    //  document.querySelector('.red').addEventListener (, function () { this.classlist.add=("text-danger") ;})
     
-}
+   
+    }
+
+     function ApplyRed() {cc.classList.add('red')};
+    
+    // Function to add a class
+   
 
 api('https://fakestoreapi.com/products','GET');
+
+// function ApplyRed(e) {e.classList.add("text-danger");
+// }
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+// slider
+function slider(url,method='GET') {
+    
+    XmlHttp.open(method, url);
+    XmlHttp.send();
+    XmlHttp.addEventListener('readystatechange', function () {
+        if (XmlHttp.readyState===4 && XmlHttp.status===200) 
+            {  JsonData =JSON.parse(XmlHttp.responseText)
+             var SliderModel=document.getElementById('model-slider')  
+             var HtmlCollection2=``
+
+       for (var slide of JsonData) {
+    
+HtmlCollection2 +=
+ `<div class="content w-50 m-auto  position-absolute z-3 " style="z-index: 1000;">
+<img src="./photo-1554995207-c18c203602cb.avif" alt="" class="w-100">
+<div class="position-relative d-flex justify-content-between top-50">
+  <button><i class="fa-solid fa-backward"></i></button>
+  <button><i class="fa-solid fa-forward"></i></button>
+</div >
+<div class="position-absolute top-0 end-0">
+  <button><i class="fa-regular fa-rectangle-xmark"></i></button></div>
+ </div>`
+ SliderModel.innerHTML = HtmlCollection2
+}}})}
+ 
+
+slider('https://fakestoreapi.com/products','GET')
 
 
 
